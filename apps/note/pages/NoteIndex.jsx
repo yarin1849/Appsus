f74c7f7377932fef442c9f1357aa7a1fdc2d9fde
Isaac Levy <isaaclevy004@gmail.com>
2025-01-22 13:38:32 +0200

    Update note service

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,19 @@
+import { noteService } from "../services/note.service.js"
+
+const {useSatate, useEffect} = React
 
 export function NoteIndex() {
+    const [notes, setNotes] = useSatate(null)
+
+    useEffect( () => {
+        loadNotes()
+    }, [])
+
+    function loadNotes() {
+        noteService.query()
+            .then(notes => console.log(notes))
+    }
+
+
     return <div>note app</div>
 }
--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,133 @@
-// note service
+import { loadFromStorage, makeId, saveToStorage } from '../../../services/util.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+
+const NOTE_KEY = 'noteDB'
+_createNotes()
+
+export const noteService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyNote,
+    getDefaultFilter,
+    getFilterFromSearchParams,
+
+}
+
+function query(filterBy = {}) {
+    return storageService.query(NOTE_KEY)
+        .then(notes => {
+            if (filterBy.txt) {
+                const regExp = new RegExp(filterBy.txt, 'i')
+                notes = notes.filter(note => regExp.test(note.vendor))
+            }
+
+            return notes
+        })
+}
+
+function get(noteId) {
+    return storageService.get(NOTE_KEY, noteId)
+        .then(note => _setNextPrevNoteId(note))
+}
+
+function remove(noteId) {
+    // return Promise.reject('Oh No!')
+    return storageService.remove(NOTE_KEY, noteId)
+}
+
+function save(note) {
+    if (note.id) {
+        return storageService.put(NOTE_KEY, note)
+    } else {
+        return storageService.post(NOTE_KEY, note)
+    }
+}
+
+function getEmptyNote(createAt = '', type = '', isPinned = false, style = {}, info = {}) {
+    return { createAt, type, isPinned, style, info }
+}
+
+function getDefaultFilter() {
+    return {
+        txt: ''
+    }
+}
+
+function _createNotes() {
+    let notes = loadFromStorage(NOTE_KEY)
+    if (!notes || !notes.length) {
+        notes = [
+            {
+                id: 'n101',
+                createdAt: 1112222,
+                type: 'NoteTxt',
+                isPinned: true,
+                style: {
+                    backgroundColor: '#00d'
+                },
+                info: {
+                    txt: 'Fullstack Me Baby!'
+                }
+            },
+            {
+                id: 'n102',
+                createdAt: 1112223,
+                type: 'NoteImg',
+                isPinned: false,
+                info: {
+                    url: 'http://some-img/me',
+                    title: 'Bobi and Me'
+                },
+                style: {
+                    backgroundColor: '#00d'
+                }
+            },
+            {
+                id: 'n103',
+                createdAt: 1112224,
+                type: 'NoteTodos',
+                isPinned: false,
+                info: {
+                    title: 'Get my stuff together',
+                    todos: [
+                        { txt: 'Driving license', doneAt: null },
+                        { txt: 'Coding power', doneAt: 187111111 }
+                    ]
+                }
+            }
+        ]
+        saveToStorage(NOTE_KEY, notes)
+    }
+}
+
+function _createNote(info) {
+    const note = getEmptyNote(info)
+    note.id = makeId()
+    return note
+}
+
+
+
+function getFilterFromSearchParams(searchParams) {
+    const txt = searchParams.get('txt') || ''
+    const minSpeed = searchParams.get('minSpeed') || ''
+    return {
+        txt,
+        minSpeed
+    }
+}
+
+
+function _setNextPrevnoteId(note) {
+    return query().then((notes) => {
+        const noteIdx = notes.findIndex((currnote) => currnote.id === note.id)
+        const nextnote = notes[noteIdx + 1] ? notes[noteIdx + 1] : notes[0]
+        const prevnote = notes[noteIdx - 1] ? notes[noteIdx - 1] : notes[notes.length - 1]
+        note.nextnoteId = nextnote.id
+        note.prevnoteId = prevnote.id
+        return note
+    })
+}
+
