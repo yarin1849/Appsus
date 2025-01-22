4587ad7e0cc95d493c94a6b0a2bc65b787d725f9
Yarin Bracha <yarin1849@gmail.com>
2025-01-22 14:22:12 +0200

    Add list and preview for emails

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,3 +1,13 @@
-export function MailList() {
-    return <div>Mail list</div>
+import { MailPreview } from "./MailPreview.jsx"
+
+export function MailList({ mails }) {
+    return (
+        <ul className="mail-list">
+            {mails.map(mail =>
+                <li key={mail.id}>
+                    <MailPreview mail={mail} />
+                </li>
+            )}
+        </ul>
+    )
 }
--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,3 +1,9 @@
-export function MailPreview() {
-    return (<div>Mail Preview</div>)
+export function MailPreview({ mail }) {
+    console.log('mail', mail)
+    return (
+        <div>
+            <h1>{mail.subject}</h1>
+            <h2>{mail.body}</h2>
+        </div>
+    )
 }
