d9129a05e878b377728d3e8fe56f43edadd763ad
Isaac Levy <isaaclevy004@gmail.com>
2025-01-25 17:35:07 +0200

    Add design

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,17 +1,23 @@
+import { AppHeader } from "../cmps/AppHeader";
 
 export function About() {
-    return <section className="about-images">
-        <h2>About Us</h2>
-        <div className="images">
-            <section>
+    return (
+        <section>
+            <AppHeader />
+            <section className="about-images">
+                <h2>About Us</h2>
+                <div className="images">
+                    <section>
 
-                <img src="./assets/img/isaac-img.png" alt="Isaac" title="Isaac" />
-                <div>Isaac Levy 28 years old from Petach Tikva</div>
+                        <img src="./assets/img/isaac-img.png" alt="Isaac" title="Isaac" />
+                        <div>Isaac Levy 28 years old from Petach Tikva</div>
+                    </section>
+                    <section>
+                        <img src="./assets/img/isaac-img.png" alt="Yarin" title="Yarin" />
+                        <div>Yarin Bracha 26 years old from Rishon leZion</div>
+                    </section>
+                </div>
             </section>
-            <section>
-                <img src="./assets/img/isaac-img.png" alt="Yarin" title="Yarin" />
-                <div>Yarin Bracha 26 years old from Rishon leZion</div>
-            </section>
-        </div>
-    </section>
+        </section>
+    )
 }
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,13 @@
 const { Route, Routes, useParams, Link } = ReactRouterDOM
 const Router = ReactRouterDOM.HashRouter
 
+import { AppHeader } from "../cmps/AppHeader";
+
 export function Home() {
     return (
 
-
+        <section>
+            <AppHeader />
             <section className="home main-layout">
                 <h1>Welcome to the Best Notes and Email Page Ever!</h1>
                 <p>
@@ -15,13 +18,14 @@ export function Home() {
                     <h2>Explore our website:</h2>
                     <nav>
                         <ul>
-                        <li><Link to="/about/"><img src='./assets/img/icons8-about-50.svg' alt="icon" /></Link></li>
-                        <li><Link to="/mail/"><img src='./assets/img/icons8-gmail.svg' alt="icon" className="gmail" /></Link></li>
-                        <li><Link to="/note"><img src='./assets/img/icons8-google-keep.svg' alt="icon" /></Link></li>
+                            <li><Link to="/about/"><img src='./assets/img/icons8-about-50.svg' alt="icon" /></Link></li>
+                            <li><Link to="/mail/"><img src='./assets/img/icons8-gmail.svg' alt="icon" className="gmail" /></Link></li>
+                            <li><Link to="/note"><img src='./assets/img/icons8-google-keep.svg' alt="icon" /></Link></li>
                         </ul>
                     </nav>
                 </section>
             </section>
+        </section>
 
     )
 
